96239ce655b3445560726b108992354a4ed3876e
Martuza Shimul <martuza.shimul@gmail.com>
2019-11-08 21:59:13 +0600

    modal resized

--- data.php
+++ data.php
@@ -10,7 +10,14 @@ if(empty($_POST['first-name'])  		||
 	echo "Please fill up the form Properly, Maybe you have missed something!";
 	return false;
    }
-	
+
+   
+$first = $_POST['first'];
+$second = $_POST['check'];
+$type = $_POST['type'];
+$third = $_POST['third'];
+$fourth = $_POST['fourth'];
+
 $first_name = $_POST['first-name'];
 $last_name = $_POST['last-name'];
 $email_address = $_POST['email'];
@@ -19,17 +26,17 @@ $postcode = $_POST['postcode'];
 	
 // Create the email and send the message
 
-// $to = 'tripkeys@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-// $email_subject = "Website Quote Engine:  $first_name";
-// $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nFirst-Name: $first_name\n\nLast-Name: $last_name\n\nEmail: $email_address\n\n Postcode:\n$postcode";
-// $headers = "From: noreply@eastsidebs.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = 'gmshimul1@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Quote Engine:  $first_name";
+$email_body = "You have received a new message from your website Quote Engine .\n\n"."Here are the details:\n\nFirst-Name: $first_name\n\nLast-Name: $last_name\n\nEmail: $email_address\n\n Postcode: $postcode";
+$headers = "From: noreply@eastsidebs.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 
-// $headers .= "Reply-To: $email_address";	
-// mail($to,$email_subject,$email_body,$headers);
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
 
-echo "<h4>The details have been sent and a member of staff will be in touch shortly!</h4>";
+// echo "<h4>The details have been sent and a member of staff will be in touch shortly!</h4>";
 
-// header("Location: thank-you.html");
+header("Location: thank-you.html");
 return true;	
 
 ?>
